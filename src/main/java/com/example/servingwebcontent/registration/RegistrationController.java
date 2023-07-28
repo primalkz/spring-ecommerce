4655ebc2b4e05901b73e5b9bd37a1497b2949906
primalkz <44187697+primalkz@users.noreply.github.com>
2023-07-28 20:17:50 +0530

    validateToken : method implemented for verification

--- src/main/java/com/example/servingwebcontent/registration/RegistrationController.java
+++ src/main/java/com/example/servingwebcontent/registration/RegistrationController.java
@@ -1,15 +1,15 @@
 package com.example.servingwebcontent.registration;
 
 import com.example.servingwebcontent.event.RegistrationCompleteEvent;
+import com.example.servingwebcontent.registration.token.VerificationToken;
+import com.example.servingwebcontent.registration.token.VerificationTokenRepository;
 import com.example.servingwebcontent.user.User;
 import com.example.servingwebcontent.user.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,18 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
     private final UserService userService;
     private final ApplicationEventPublisher publisher;
+    private final VerificationTokenRepository tokenRepository;
     @PostMapping
 
-    public String registerUser(RegistrationRequest registrationRequest, final HttpServletRequest request) {
+    public String registerUser(@RequestBody RegistrationRequest registrationRequest, final HttpServletRequest request) {
         User user = userService.registerUser(registrationRequest);
         // publishing the registration event :)
         publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
         return "Done! Please, check your email for verification.";
     }
+    @GetMapping("/verifyEmail")
+    public String verifyEmail(@RequestParam("token") String token){
+        VerificationToken theToken = tokenRepository.findByToken(token);
+        if (theToken.getUser().isEnabled()){
+            return "This account is already verified, please login.";
+        }
+        String verificationResult = userService.validateToken(token);
+        if(verificationResult.equalsIgnoreCase("valid")){
+            return "Email verified successfully. Now you can login to your account";
+        }
+        return "Invalid verification token";
+
+    }
 
 
 
-    public Object applicationUrl(HttpServletRequest request) {
+    public String applicationUrl(HttpServletRequest request) {
         return "http://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
     }
 
--- src/main/java/com/example/servingwebcontent/user/IUserService.java
+++ src/main/java/com/example/servingwebcontent/user/IUserService.java
@@ -1,6 +1,7 @@
 package com.example.servingwebcontent.user;
 
 import com.example.servingwebcontent.registration.RegistrationRequest;
+import com.example.servingwebcontent.registration.token.VerificationToken;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +10,6 @@ public interface IUserService {
     List<User> getUsers();
     User registerUser(RegistrationRequest request);
     Optional<User> findByEmail(String email);
+
+    String validateToken(String theToken);
 }
