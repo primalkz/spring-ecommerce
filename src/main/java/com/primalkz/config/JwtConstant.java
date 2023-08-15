4930f6c304a18c812592845b0010043d5312c5f8
primalkz <44187697+primalkz@users.noreply.github.com>
2023-08-15 23:06:42 +0530

    Jwt Validation request

---
+++ src/main/java/com/primalkz/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.primalkz.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="asasdahsoudahisncaoi4nqiwenq00a@aisjdiai0aszxca";
+    public static final String JWT_HEADER="Authorization";
+}
