47e521e0c8fafa649e11747ec252723d6b3e01e0
primalkz <44187697+primalkz@users.noreply.github.com>
2023-08-22 13:28:26 +0530

    signin and signup changes

--- src/main/java/com/primalkz/controller/AuthController.java
+++ src/main/java/com/primalkz/controller/AuthController.java
@@ -5,10 +5,18 @@ import com.primalkz.config.JwtProvider;
 import com.primalkz.exception.UserException;
 import com.primalkz.model.User;
 import com.primalkz.repository.UserRepository;
+import com.primalkz.request.LoginRequest;
+import com.primalkz.response.AuthResponse;
+import com.primalkz.service.CustomerUserServiceImplementation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +27,20 @@ public class AuthController {
 
     private UserRepository userRepository;
     private JwtProvider jwtProvider;
+    private PasswordEncoder passwordEncoder;
+    private CustomerUserServiceImplementation customUserService;
 
-    public AuthController(UserRepository userRepository) {
+    public AuthController(UserRepository userRepository,
+                          CustomerUserServiceImplementation customUserService,
+                          PasswordEncoder passwordEncoder,
+                          JwtProvider jwtProvider) {
         this.userRepository=userRepository;
+        this.customUserService=customUserService;
+        this.passwordEncoder=passwordEncoder;
+        this.jwtProvider=jwtProvider;
 
     }
-
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse>createUserHandler(@RequestBody User user)throws UserException {
         String email=user.getEmail();
         String password=user.getPassword();
@@ -38,19 +54,54 @@ public class AuthController {
             throw new UserException("Email is already in use with another account");
         }
 
+
         User createdUser=new User();
         createdUser.setEmail(email);
-        createdUser.setPassword(password);
+        createdUser.setPassword(passwordEncoder.encode(password));
         createdUser.setFirstName(firstName);
         createdUser.setLastName(lastName);
 
         User savedUser=userRepository.save(createdUser);
 
-        Authentication authentication=new UsernamePasswordAuthenticationToken(email, password);
+        Authentication authentication=new UsernamePasswordAuthenticationToken(savedUser.getEmail(), savedUser.getPassword());
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String token = jwtProvider.generateToken(authentication);
 
+        AuthResponse authResponse=new AuthResponse();
+        authResponse.setJwt(token);
+        authResponse.setMessage("Signup Success");
 
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse>loginUserHandler(@RequestBody LoginRequest loginRequest){
+        String username=loginRequest.getEmail();
+        String password=loginRequest.getPassword();
+
+        Authentication authentication=authenticate(username,password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse=new AuthResponse();
+        authResponse.setJwt(token);
+        authResponse.setMessage("Signin Success");
+
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserService.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid Username...");
+        }
+
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid Password...");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
     }
 }
--- src/main/java/com/primalkz/service/CustomerUserServiceImplementation.java
+++ src/main/java/com/primalkz/service/CustomerUserServiceImplementation.java
@@ -1,12 +1,12 @@
 package com.primalkz.service;
 
 import com.primalkz.repository.UserRepository;
+import com.primalkz.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +24,7 @@ public class CustomerUserServiceImplementation implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user=userRepository.findByEmail(username);
-        if(user == null){
+        if(user==null){
             throw new UsernameNotFoundException("user not found with provided email - "+ username);
         }
 
